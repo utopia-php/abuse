6ffb076536b7f0408d06c71b7744e4f8c5c1ac6a
Damodar Lohani <dlohani48@gmail.com>
2021-05-17 16:41:36 +0545

    add id to the data array as well

--- src/Abuse/Adapters/TimeLimit.php
+++ src/Abuse/Adapters/TimeLimit.php
@@ -187,6 +187,7 @@ class TimeLimit implements Adapter
             var_dump($existing[0]->getId());
             $this->db->updateDocument(TimeLimit::COLLECTION, $existing[0]->getId(), new Document(array_merge($data, [
                 '_count' => $existing[0]->getAttribute('_count',0) + 1,
+                '$id' => $existing[0]->getId(),
             ])));
         } else {
             //create
