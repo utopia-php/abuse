907f36118e2b68042eec7671a5700691d9984f78
Damodar Lohani <dlohani48@gmail.com>
2021-05-17 16:32:31 +0545

    var dump existing

--- src/Abuse/Adapters/TimeLimit.php
+++ src/Abuse/Adapters/TimeLimit.php
@@ -183,6 +183,7 @@ class TimeLimit implements Adapter
 
         if (count($existing) == 1) {
             //update
+            var_dump($existing);
             $this->db->updateDocument(TimeLimit::COLLECTION, $existing[0]->getId(), new Document(array_merge($data, [
                 '_count' => $existing[0]->getAttribute('_count',0) + 1,
             ])));
