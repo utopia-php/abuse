cc811b14d550d389b134d897ecef06e8d278db53
fogelito <fogelshmuel@gmail.com>
2022-07-19 13:14:58 +0300

    MariaDB::getPdoAttributes

--- tests/Abuse/AbuseTest.php
+++ tests/Abuse/AbuseTest.php
@@ -19,6 +19,7 @@ use Utopia\Abuse\Abuse;
 use Utopia\Abuse\Adapters\TimeLimit;
 use Utopia\Cache\Adapter\None as NoCache;
 use Utopia\Cache\Cache;
+use Utopia\Database\Adapter\MariaDB;
 use Utopia\Database\Adapter\MySQL;
 use Utopia\Database\Database;
 use Utopia\Database\DateTime;
@@ -38,14 +39,7 @@ class AbuseTest extends TestCase
         $dbPort = '3306';
         $dbPass = 'password';
 
-        $pdo = new PDO("mysql:host={$dbHost};port={$dbPort};charset=utf8mb4", $dbUser, $dbPass, [
-            PDO::ATTR_TIMEOUT => 3, // Seconds
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => true,
-            PDO::ATTR_STRINGIFY_FETCHES => true,
-        ]);
+        $pdo = new PDO("mysql:host={$dbHost};port={$dbPort};charset=utf8mb4", $dbUser, $dbPass, MariaDB::getPdoAttributes());
 
         $db = new Database(new MySQL($pdo), new Cache(new NoCache()));
         $db->setDefaultDatabase('utopiaTests');
