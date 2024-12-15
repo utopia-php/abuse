c474e75327153e9824457aae52edd379550cce3b
Christy Jacob <christyjacob4@gmail.com>
2024-12-15 04:33:59 +0000

    chore: add redis cluster adapter

---
+++ src/Abuse/Adapters/RedisCluster/TimeLimit.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Utopia\Abuse\Adapters\Redis;
+
+use RedisCluster as Client;
+use Utopia\Abuse\Adapters\Redis\TimeLimit as RedisTimeLimit;
+
+class TimeLimit extends RedisTimeLimit
+{
+    public const NAMESPACE = 'abuse';
+
+    /**
+     * @var Client
+     */
+    protected Client $redis;
+
+    public function __construct(string $key, int $limit, int $seconds, Client $redis)
+    {
+        $this->redis = $redis;
+        $this->key = $key;
+        $time = (int) \date('U', (int) (\floor(\time() / $seconds)) * $seconds);
+        $this->time = strval($time);
+        $this->limit = $limit;
+    }
+}
