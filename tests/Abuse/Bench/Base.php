cb92c592e1accca890644b9989540827ad2c5e8b
Jake Barnby <jakeb994@gmail.com>
2025-10-20 18:59:29 +1300

    Fix PSR autoload

--- tests/Abuse/Bench/Base.php
+++ tests/Abuse/Bench/Base.php
@@ -2,12 +2,12 @@
 
 namespace Utopia\Tests\Bench;
 
-use Utopia\Abuse\Abuse;
-use Utopia\Abuse\Adapters\TimeLimit;
 use PhpBench\Attributes\BeforeMethods;
 use PhpBench\Attributes\Iterations;
 use PhpBench\Attributes\OutputMode;
 use PhpBench\Attributes\OutputTimeUnit;
+use Utopia\Abuse\Abuse;
+use Utopia\Abuse\Adapters\TimeLimit;
 
 abstract class Base
 {
@@ -15,17 +15,17 @@ abstract class Base
     protected TimeLimit $adapter;
 
     #[BeforeMethods('setUp')]
-    #[Iterations([20,30,50])]
+    #[Iterations([20, 30, 50])]
     #[OutputMode('throughput')]
     #[OutputTimeUnit('millisecond')]
     public function benchTimelimit(): void
     {
         $ip = '';
         for ($i = 0; $i < 4; $i++) {
-            $sub = rand(0, 255);
+            $sub = \rand(0, 255);
             $ip .= $sub . '.';
         };
-        $ip = rtrim($ip, '.');
+        $ip = \rtrim($ip, '.');
         $this->adapter->setParam('{{ip}}', $ip);
         $this->abuse->check();
     }
--- tests/Abuse/Bench/RedisCluster.php
+++ tests/Abuse/Bench/RedisCluster.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace Abuse\Bench;
+namespace Utopia\Tests\Bench;
 
 use RedisCluster as Client;
 use Utopia\Abuse\Abuse;
-use Utopia\Abuse\Adapters\TimeLimit\RedisCluster;
-use Utopia\Tests\Bench\Base;
+use Utopia\Abuse\Adapters\TimeLimit\RedisCluster as RedisClusterAdapter;
 
-final class RedisClusterBench extends Base
+final class RedisCluster extends Base
 {
     protected Client $redis;
 
@@ -17,7 +16,7 @@ final class RedisClusterBench extends Base
     public function setUp(): void
     {
         $this->redis = new Client(null, ['redis-cluster-0:6379', 'redis-cluster-1:6379', 'redis-cluster-2:6379', 'redis-cluster-3:6379']);
-        $this->adapter = new RedisCluster('login-attempt-from-{{ip}}', 3, 60 * 5, $this->redis);
+        $this->adapter = new RedisClusterAdapter('login-attempt-from-{{ip}}', 3, 60 * 5, $this->redis);
         $this->abuse = new Abuse($this->adapter);
     }
 }
